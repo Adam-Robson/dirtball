3f730e113544b6be2b321d0c84469c4e40657eca
Adam Robson <adamrayrobson@gmail.com>
2024-12-07 10:37:25 -0800

    add date utilities

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.info("hello dirtball");
+import {
+  formatDate,
+  parseDate,
+  addDaysToDate,
+  isNotWeekday,
+  businessDaysBetween,
+  isValidDate
+} from './utils';
+
+formatDate(new Date(), 'YYYY-MM-DD');
+parseDate('2021-01-01');
+addDaysToDate(new Date(), 5);
+isNotWeekday(new Date());
+businessDaysBetween(new Date(), new Date());
+isValidDate(new Date());
